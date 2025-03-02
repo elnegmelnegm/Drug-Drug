54dff1d2b17d511a68b8a1ffa7727781215d42c6
elnegmelnegm <48047676+elnegmelnegm@users.noreply.github.com>
2025-03-02 09:20:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def generate_gemini_text_response(text_model, user_input):
 
 # Display header and logos
 st.markdown('''<img src="https://www.edaegypt.gov.eg/media/wc3lsydo/group-287.png" width="250" height="100">''', unsafe_allow_html=True)
-st.markdown('''Powered by Google AI <img src="https://seeklogo.com/images/G/google-ai-logo-996E85F6FD-seeklogo.com.png" width="20" height="20"> Streamlit <img src="https://global.discourse-cdn.com/business7/uploads/streamlit/original/2X/f/f0d0d26db1f2d99da8472951c60e5a1b782eb6fe.png" width="22" height="22"> Python <img src="https://i.ibb.co/wwCs096/nn-1-removebg-preview-removebg-preview.png" width="22" height="22">''', unsafe_allow_html=True)
+st.markdown('''Powered by Google AI <img src="https://seeklogo.com/images/G/google-ai-logo-996E85F6FD-seeklogo.com.png" width="20" height="20"> Streamlit <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fmedium.com%2Fstreamlit%2Fcreating-repeatable-items-in-streamlit-cb8b6264e1e6&psig=AOvVaw0Mm9y04s_WlxJ9mIu38yNg&ust=1740986251909000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKjY7rbu6osDFQAAAAAdAAAAABAJ" width="22" height="22"> Python <img src="https://i.ibb.co/wwCs096/nn-1-removebg-preview-removebg-preview.png" width="22" height="22">''', unsafe_allow_html=True)
 
 # Load the text model
 text_model = load_text_model()
